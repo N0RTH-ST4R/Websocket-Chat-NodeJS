1392834c4f9a5cdddf692068525c53cd3da7b188
Waffle <74199677+WaffleDragon86@users.noreply.github.com>
2022-05-26 17:19:09 +0000

    add html and css

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+import {createServer}from 'http';
+import staticHandler from 'serve=handler';
+import ws, {WebSocketServer} from 'ws';
+const server=createServer((req,res)=>{
+    return staticHandler(req,res,{public: 'public'})
+});
+const wss=new WebSocketServer({server})
+wss.on('connection',(client)=>{
+    console.log("CLIENT CONNECTED")
+    client.on('message',(msg)=>{
+        console.log(`Message:${msg}`);
+        broadcast(msg)
+    })
+})
+function broadcast(msg){
+    for(const client of wss.clients){
+        if(client.readyState===ws.OPEN){
+            client.send(msg)
+        }
+    }
+}
+server.listen(process.argv[2]||8080,()=>{
+    console.log(`SERVER LISTENING`);
+})
